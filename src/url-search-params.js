4c116a1883519d8b839538c1c8b6b0b16540628f
Christopher Dow <cdow2@godaddy.com>
2019-10-19 11:02:01 -0700

    Change the way we import url-search-params to work around an issue with Webpack
    See this for more details: https://github.com/ungap/url-search-params/issues/8

--- src/url-search-params.js
+++ src/url-search-params.js
@@ -1,6 +1,6 @@
 let URLSearchParams = (typeof window !== 'undefined') && window.URLSearchParams;
 if (!URLSearchParams) {
-  URLSearchParams = require('@ungap/url-search-params');
+  URLSearchParams = require('@ungap/url-search-params/cjs');
 }
 
 module.exports = URLSearchParams;
