5e2dc96c5513be7757317e30c33a3b49d315604e
Michael Mason <mmason2@godaddy.com>
2022-09-14 11:52:39 -0700

    feat: update jest deps, remove search params polyfill, remove obsolete tests

--- src/url-search-params.js
+++ src/url-search-params.js
@@ -1,6 +1,1 @@
-let URLSearchParams = (typeof window !== 'undefined') && window.URLSearchParams;
-if (!URLSearchParams) {
-  URLSearchParams = require('@ungap/url-search-params/cjs');
-}
-
-module.exports = URLSearchParams;
+module.exports = (typeof window !== 'undefined') ? window.URLSearchParams : URLSearchParams;
